415553e5a059a068bd421752cf25249b74e95b4c
Haden Santefort <hsantefort12@gmail.com>
2019-12-09 15:51:41 -0500

    Removed old variable

--- calc.js
+++ calc.js
@@ -52,7 +52,6 @@
  }
 
  function determineBits(binNum) {
-    //var bitsValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     var bitsValues = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
     var i = 0;
     [...binNum].forEach(element => {
